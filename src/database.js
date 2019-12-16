532835c1a002db37b83f7e7d6fe781d8e58d6f9a
Jonathan <jrgalvisg@unal.edu.co>
2019-12-16 18:08:08 -0500

    node

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://34.68.15.162/attatchment_db', {
+mongoose.connect('mongodb://34.68.15.162:27017/attatchment_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
